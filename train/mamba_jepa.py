c8864cc10e1e9d78d83c956b40cb96a7ff1f6ed6
redEddie <eddie36@naver.com>
2026-02-26 20:50:41 +0900

    시퀸스의 모든 지점을 context length로 삼음;

--- train/mamba_jepa.py
+++ train/mamba_jepa.py
@@ -27,21 +27,18 @@ class MambaJEPA(nn.Module):
             param.requires_grad = False
             
         # 3. Predictor (MLP)
+        # Input: [Hidden_State(t), delta_t]
         self.predictor = nn.Sequential(
             nn.Linear(config.d_model + 1, config.d_model * 2), 
             nn.ReLU(),
             nn.Linear(config.d_model * 2, config.d_model)
         )
 
-        # EMA Settings
         self.base_momentum = base_momentum
         self.final_momentum = final_momentum
         self.current_momentum = base_momentum
 
     def update_ema(self, step, max_steps):
-        """
-        Adjust EMA momentum based on training progress.
-        """
         self.current_momentum = self.base_momentum + \
             (self.final_momentum - self.base_momentum) * min(1.0, step / max_steps)
             
@@ -49,18 +46,17 @@ class MambaJEPA(nn.Module):
             for c_param, t_param in zip(self.context_model.parameters(), self.target_model.parameters()):
                 t_param.data.mul_(self.current_momentum).add_(c_param.data, alpha=1 - self.current_momentum)
 
-    def forward(self, images, lowdim, mask_ratio=0.6):
+    def forward(self, images, lowdim, num_context_points=10, prediction_horizon=16):
         """
         images: dict of {cam_name: [B, L, 3, H, W]}
         lowdim: [B, L, D]
+        num_context_points: 한 궤적 내에서 학습할 'Context 종료 지점'의 개수
+        prediction_horizon: 각 지점에서 미래로 몇 스텝을 예측할지
         """
         B, L, _ = lowdim.shape
         device = lowdim.device
         
-        # 1. Split sequence (Context vs Target)
-        context_len = max(1, int(L * (1 - mask_ratio)))
-        
-        # 2. Target Encoding (Full sequence)
+        # 1. Target Encoding (전체 시퀀스의 표현형을 먼저 추출)
         with torch.no_grad():
             target_repr_list = []
             h_target = self.target_model.init_hidden_states(B, device)
@@ -68,34 +64,52 @@ class MambaJEPA(nn.Module):
                 imgs_t = {cam: images[cam][:, t] for cam in self.target_model.camera_names}
                 _, h_target, hidden = self.target_model.step(lowdim[:, t], imgs_t, h_target, return_repr=True)
                 target_repr_list.append(hidden.unsqueeze(1)) 
-            
-            target_repr = torch.cat(target_repr_list, dim=1) 
+            target_repr = torch.cat(target_repr_list, dim=1) # [B, L, d_model]
 
-        # 3. Context Encoding (Only context part)
+        # 2. Context Encoding (전체 시퀀스를 훑으며 각 시점의 Hidden State 추출)
         context_repr_list = []
         pred_actions_list = []
         h_context = self.context_model.init_hidden_states(B, device)
-        for t in range(context_len):
+        for t in range(L):
             imgs_t = {cam: images[cam][:, t] for cam in self.context_model.camera_names}
             pred_act, h_context, hidden = self.context_model.step(lowdim[:, t], imgs_t, h_context, return_repr=True)
             context_repr_list.append(hidden.unsqueeze(1))
             pred_actions_list.append(pred_act.unsqueeze(1))
         
-        last_context_repr = context_repr_list[-1].squeeze(1) 
-        # For evaluation/logging, we might want the actions predicted during context
-        pred_actions = torch.cat(pred_actions_list, dim=1) # [B, context_len, future_steps, action_dim]
+        context_reprs = torch.cat(context_repr_list, dim=1) # [B, L, d_model]
+        pred_actions = torch.cat(pred_actions_list, dim=1) # [B, L, future_steps, action_dim]
 
-        # 4. Predictor: Predict future Target representations
+        # 3. Predictor Training (여러 시점을 Context로 삼아 학습)
+        # 너무 촘촘하면 계산량이 많으므로 num_context_points 만큼 무작위 지점을 선택
+        # 단, 미래 데이터가 남아있는 지점들 중에서 선택 (1 ~ L - 2)
+        if L < 5: # 궤적이 너무 짧으면 처리 불가
+            return torch.tensor(0.0, device=device, requires_grad=True), pred_actions
+
         loss = 0
-        num_targets = L - context_len
-        if num_targets > 0:
-            for t_future in range(context_len, L):
-                delta_t = torch.tensor([[float(t_future - context_len + 1)]], device=device).expand(B, 1)
-                pred_input = torch.cat([last_context_repr, delta_t], dim=-1)
-                predicted_repr = self.predictor(pred_input)
-                actual_repr = target_repr[:, t_future]
-                loss += F.mse_loss(predicted_repr, actual_repr)
+        actual_points = 0
+        
+        # Context 종료 지점 샘플링
+        indices = torch.randperm(L - 1)[:num_context_points].tolist()
+        
+        for t_ctx in indices:
+            ctx_v = context_reprs[:, t_ctx] # t_ctx 시점까지의 누적 정보
             
-            return loss / num_targets, pred_actions
+            # 해당 시점으로부터 미래 prediction_horizon 만큼 예측
+            max_h = min(prediction_horizon, L - 1 - t_ctx)
+            if max_h <= 0: continue
+            
+            for h in range(1, max_h + 1):
+                t_future = t_ctx + h
+                delta_t = torch.tensor([[float(h)]], device=device).expand(B, 1)
+                
+                pred_input = torch.cat([ctx_v, delta_t], dim=-1)
+                predicted_repr = self.predictor(pred_input)
+                actual_target_v = target_repr[:, t_future]
+                
+                loss += F.mse_loss(predicted_repr, actual_target_v)
+                actual_points += 1
+        
+        if actual_points > 0:
+            return loss / actual_points, pred_actions
         else:
             return torch.tensor(0.0, device=device, requires_grad=True), pred_actions
--- train/train_jepa.py
+++ train/train_jepa.py
@@ -1,19 +1,37 @@
 import os
 import torch
 from torch import nn
-import random
+import numpy as np
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Sampler, BatchSampler
 import torch.nn.functional as F
 from pytorch_lightning import seed_everything
 from train.metric_M import my_Metric
 from train.scaler_M import Scaler
-from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
-
 from train.M_dataset import MambaSequenceDataset
 from train.mamba_jepa import MambaJEPA
 from train.mamba_policy import MambaConfig
 from pytorch_lightning.loggers import CSVLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
+class TrajectoryBatchSampler(Sampler):
+    """
+    데이터셋의 cum_lengths를 이용하여 궤적 단위로 인덱스를 묶어서 반환하는 샘플러.
+    이를 통해 DataLoader가 1회 호출될 때 하나의 전체 궤적을 반환하게 함.
+    """
+    def __init__(self, cum_lengths, shuffle=False):
+        self.cum_lengths = cum_lengths
+        self.shuffle = shuffle
+        self.traj_indices = list(range(len(cum_lengths) - 1))
+
+    def __iter__(self):
+        if self.shuffle:
+            np.random.shuffle(self.traj_indices)
+        for i in self.traj_indices:
+            yield list(range(self.cum_lengths[i], self.cum_lengths[i+1]))
+
+    def __len__(self):
+        return len(self.traj_indices)
 
 class LitMambaJEPA(pl.LightningModule):
     def __init__(self, config: MambaConfig, scaler: Scaler):
@@ -25,23 +43,6 @@ class LitMambaJEPA(pl.LightningModule):
         
         self.lr = 1e-4
         self.weight_decay = 1e-4
-        
-        self.prev_traj_idx = -1
-        self.train_sequence_loss = 0.0
-        self.val_sequence_loss = 0.0
-        
-        self.train_total_loss = 0.0
-        self.train_total_steps = 0
-        self.val_total_loss = 0.0
-        self.val_total_steps = 0
-        
-        self.automatic_optimization = False
-        
-        # Buffer to store trajectory data
-        self.current_traj_rgb = []
-        self.current_traj_lowdim = []
-        self.current_traj_actions = []
-        
         self.total_steps = 0
         self.max_training_steps = 100000 
 
@@ -52,161 +53,77 @@ class LitMambaJEPA(pl.LightningModule):
         self.std_agl2_4 = 0.1414; self.std_agl2_5 = 0.3066; self.std_agl2_6 = 0.2251
         self.std_grip1 = 0.2553; self.std_grip2 = 0.2475
 
-    def training_step(self, batch, batch_idx):
-        optimizer = self.optimizers()
+    def _prepare_batch(self, batch):
+        # batch: DataLoader가 TrajectoryBatchSampler를 통해 묶어준 [L, ...] 형태의 데이터
         rgb = batch['rgb']
         lowdim = batch['lowdim']
-        traj_idx = batch['traj_idx'].item()
-
-        # Noise injection (from train.py)
+        
+        # Noise injection (Sequence level)
         noise_agl = 0.02
-        with torch.no_grad():
-            for key, std in [('agl_1', self.std_agl_1), ('agl_2', self.std_agl_2), ('agl_3', self.std_agl_3),
-                             ('agl_4', self.std_agl_4), ('agl_5', self.std_agl_5), ('agl_6', self.std_agl_6),
-                             ('agl2_1', self.std_agl2_1), ('agl2_2', self.std_agl2_2), ('agl2_3', self.std_agl2_3),
-                             ('agl2_4', self.std_agl2_4), ('agl2_5', self.std_agl2_5), ('agl2_6', self.std_agl2_6)]:
-                if key in lowdim:
-                    lowdim[key] += torch.randn_like(lowdim[key]) * noise_agl * std
+        for key, std in [('agl_1', self.std_agl_1), ('agl_2', self.std_agl_2), ('agl_3', self.std_agl_3),
+                         ('agl_4', self.std_agl_4), ('agl_5', self.std_agl_5), ('agl_6', self.std_agl_6),
+                         ('agl2_1', self.std_agl2_1), ('agl2_2', self.std_agl2_2), ('agl2_3', self.std_agl2_3),
+                         ('agl2_4', self.std_agl2_4), ('agl2_5', self.std_agl2_5), ('agl2_6', self.std_agl2_6)]:
+            if key in lowdim:
+                lowdim[key] += torch.randn_like(lowdim[key]) * noise_agl * std
 
-        # Handle trajectory change
-        if traj_idx != self.prev_traj_idx and self.prev_traj_idx != -1:
-            self.process_trajectory(optimizer, mode='train')
-            self.prev_traj_idx = traj_idx
-        elif self.prev_traj_idx == -1:
-            self.prev_traj_idx = traj_idx
-
-        # Data preprocessing
         lowdim_norm = self.scaler.normalize(lowdim)
+        
+        # Concat lowdim [1, L, 14] -> Batch size is 1 trajectory
         concat_lowdim = torch.cat([
             lowdim_norm['agl_1'], lowdim_norm['agl_2'], lowdim_norm['agl_3'], lowdim_norm['agl_4'], 
             lowdim_norm['agl_5'], lowdim_norm['agl_6'], lowdim_norm['gripper_pos'],
             lowdim_norm['agl2_1'], lowdim_norm['agl2_2'], lowdim_norm['agl2_3'], 
             lowdim_norm['agl2_4'], lowdim_norm['agl2_5'], lowdim_norm['agl2_6'], 
             lowdim_norm['gripper_pos2']
-        ], dim=1)
+        ], dim=-1).unsqueeze(0)
 
-        # Collect target actions for metrics
-        actions = torch.cat([
+        # RGB Dict transformation: {cam: [1, L, 3, H, W]}
+        rgb_seq = {cam: rgb[cam].unsqueeze(0) for cam in rgb}
+
+        # Target actions [1, L, 16, 14]
+        gt_actions = torch.cat([
             lowdim_norm['agl_1_act'], lowdim_norm['agl_2_act'], lowdim_norm['agl_3_act'],
             lowdim_norm['agl_4_act'], lowdim_norm['agl_5_act'], lowdim_norm['agl_6_act'],
             lowdim_norm['gripper_act'],
             lowdim_norm['agl2_1_act'], lowdim_norm['agl2_2_act'], lowdim_norm['agl2_3_act'],
             lowdim_norm['agl2_4_act'], lowdim_norm['agl2_5_act'], lowdim_norm['agl2_6_act'],
             lowdim_norm['gripper_act2']
-        ], dim=2) # [1, 16, 14]
+        ], dim=-1).unsqueeze(0)
 
-        # Accumulate (moving to CPU to save GPU memory)
-        self.current_traj_rgb.append({cam: rgb[cam].cpu() for cam in rgb})
-        self.current_traj_lowdim.append(concat_lowdim.cpu())
-        self.current_traj_actions.append(actions.cpu())
+        return rgb_seq, concat_lowdim, gt_actions
 
-        return None
-
-    def process_trajectory(self, optimizer, mode='train'):
-        if not self.current_traj_rgb:
-            return
-
-        L = len(self.current_traj_rgb)
-        # Move to GPU for processing
-        rgb_seq = {}
-        for cam in self.current_traj_rgb[0].keys():
-            rgb_seq[cam] = torch.stack([d[cam] for d in self.current_traj_rgb], dim=1).to(self.device)
+    def training_step(self, batch, batch_idx):
+        rgb, lowdim, _ = self._prepare_batch(batch)
         
-        lowdim_seq = torch.stack(self.current_traj_lowdim, dim=1).to(self.device) 
-        gt_actions_seq = torch.stack(self.current_traj_actions, dim=1).to(self.device) # [1, L, 16, 14]
-
-        # Forward JEPA
-        if mode == 'train':
-            jepa_loss, pred_actions = self.jepa(rgb_seq, lowdim_seq)
-            if jepa_loss.requires_grad:
-                self.manual_backward(jepa_loss)
-                torch.nn.utils.clip_grad_norm_(self.jepa.parameters(), max_norm=1.0)
-                optimizer.step()
-                optimizer.zero_grad()
-            self.jepa.update_ema(self.total_steps, self.max_training_steps)
-            self.total_steps += 1
-            
-            # 컬럼명을 jepa_loss로 변경하고, ema_momentum을 추가 기록
-            self.log("jepa_loss", jepa_loss.item(), prog_bar=True, on_step=True, on_epoch=True, batch_size=1)
-            self.log("ema_momentum", self.jepa.current_momentum, on_step=True, on_epoch=False, batch_size=1)
-            
-            self.train_total_loss += jepa_loss.item() * L
-            self.train_total_steps += L
-        else:
-            with torch.no_grad():
-                jepa_loss, pred_actions = self.jepa(rgb_seq, lowdim_seq)
-            self.log("val_loss", jepa_loss.item(), prog_bar=True, on_step=False, on_epoch=True, batch_size=1)
-            self.val_total_loss += jepa_loss.item() * L
-            self.val_total_steps += L
-            
-            # Update metrics (only on context part where actions are predicted)
-            # pred_actions: [1, context_len, 16, 14]
-            # gt_actions_seq: [1, L, 16, 14]
-            ctx_len = pred_actions.shape[1]
-            # Denormalize for metrics
-            pred_actions_denorm = self.denormalize(pred_actions.view(-1, 16, 14))
-            gt_actions_denorm = self.denormalize(gt_actions_seq[:, :ctx_len].contiguous().view(-1, 16, 14))
-            self.metric.update(pred_actions_denorm, gt_actions_denorm)
-
-        # Reset
-        self.current_traj_rgb = []
-        self.current_traj_lowdim = []
-        self.current_traj_actions = []
+        loss, _ = self.jepa(rgb, lowdim)
+        
+        self.jepa.update_ema(self.total_steps, self.max_training_steps)
+        self.total_steps += 1
+        
+        self.log("train_loss", loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log("ema_momentum", self.jepa.current_momentum, on_step=True, on_epoch=False)
+        
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        rgb = batch['rgb']
-        lowdim = batch['lowdim']
-        traj_idx = batch['traj_idx'].item()
-
-        if traj_idx != self.prev_traj_idx and self.prev_traj_idx != -1:
-            self.process_trajectory(None, mode='val')
-            self.prev_traj_idx = traj_idx
-        elif self.prev_traj_idx == -1:
-            self.prev_traj_idx = traj_idx
-
-        lowdim_norm = self.scaler.normalize(lowdim)
-        concat_lowdim = torch.cat([
-            lowdim_norm['agl_1'], lowdim_norm['agl_2'], lowdim_norm['agl_3'], lowdim_norm['agl_4'], 
-            lowdim_norm['agl_5'], lowdim_norm['agl_6'], lowdim_norm['gripper_pos'],
-            lowdim_norm['agl2_1'], lowdim_norm['agl2_2'], lowdim_norm['agl2_3'], 
-            lowdim_norm['agl2_4'], lowdim_norm['agl2_5'], lowdim_norm['agl2_6'], 
-            lowdim_norm['gripper_pos2']
-        ], dim=1)
+        rgb, lowdim, gt_actions = self._prepare_batch(batch)
         
-        actions = torch.cat([
-            lowdim_norm['agl_1_act'], lowdim_norm['agl_2_act'], lowdim_norm['agl_3_act'],
-            lowdim_norm['agl_4_act'], lowdim_norm['agl_5_act'], lowdim_norm['agl_6_act'],
-            lowdim_norm['gripper_act'],
-            lowdim_norm['agl2_1_act'], lowdim_norm['agl2_2_act'], lowdim_norm['agl2_3_act'],
-            lowdim_norm['agl2_4_act'], lowdim_norm['agl2_5_act'], lowdim_norm['agl2_6_act'],
-            lowdim_norm['gripper_act2']
-        ], dim=2)
-
-        self.current_traj_rgb.append({cam: rgb[cam].cpu() for cam in rgb})
-        self.current_traj_lowdim.append(concat_lowdim.cpu())
-        self.current_traj_actions.append(actions.cpu())
-        return None
-
-    def on_train_epoch_end(self):
-        optimizer = self.optimizers()
-        if self.current_traj_rgb:
-            self.process_trajectory(optimizer, mode='train')
+        loss, pred_actions = self.jepa(rgb, lowdim)
         
-        # Step the scheduler
-        sch = self.lr_schedulers()
-        if sch is not None:
-            sch.step()
-            
-        self.log("train_epoch_loss", self.train_total_loss / self.train_total_steps if self.train_total_steps > 0 else 0.0, prog_bar=True)
-        self.train_total_loss = 0.0; self.train_total_steps = 0
+        self.log("val_loss", loss, prog_bar=True, on_epoch=True)
+        
+        # Metrics update
+        ctx_len = pred_actions.shape[1]
+        pred_actions_denorm = self.denormalize(pred_actions.reshape(-1, 16, 14))
+        gt_actions_denorm = self.denormalize(gt_actions[:, :ctx_len].reshape(-1, 16, 14))
+        self.metric.update(pred_actions_denorm, gt_actions_denorm)
+        
+        return loss
 
     def on_validation_epoch_end(self):
-        if self.current_traj_rgb:
-            self.process_trajectory(None, mode='val')
         self.log_dict(self.metric.compute(), prog_bar=True)
         self.metric.reset()
-        self.log("val_epoch_loss", self.val_total_loss / self.val_total_steps if self.val_total_steps > 0 else 0.0, prog_bar=True)
-        self.val_total_loss = 0.0; self.val_total_steps = 0
 
     def denormalize(self, actions):
         arm1_dict = {
@@ -228,12 +145,20 @@ class LitMambaJEPA(pl.LightningModule):
             arm2_denorm['agl2_1_act'], arm2_denorm['agl2_2_act'], arm2_denorm['agl2_3_act'],
             arm2_denorm['agl2_4_act'], arm2_denorm['agl2_5_act'], arm2_denorm['agl2_6_act'],
             arm2_denorm['gripper_act2']
-        ], dim=2)
+        ], dim=-1)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.jepa.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200, eta_min=1e-6)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=1e-6)
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
+
+def limit_dataset(dataset, max_trajs):
+    """기존 데이터셋 객체의 내부 리스트를 슬라이싱하여 궤적 수를 제한함"""
+    if len(dataset.records) > max_trajs:
+        dataset.records = dataset.records[:max_trajs]
+        dataset.lengths = dataset.lengths[:max_trajs]
+        dataset.cum_lengths = np.cumsum([0] + dataset.lengths)
+    return dataset
 
 def main():
     seed_everything(42)
@@ -241,36 +166,44 @@ def main():
     config.camera_names = ['top']
     config.img_size = (640, 480)
     root_path = "/home/jeonchanwook/MTIL/transfer.100"
+    
+    # 1. 데이터셋 생성 (기존 MambaSequenceDataset 그대로 사용)
     train_dataset = MambaSequenceDataset(root_dir=root_path, mode="train", selected_cameras=config.camera_names)
     val_dataset = MambaSequenceDataset(root_dir=root_path, mode="test", selected_cameras=config.camera_names)
+    
+    # 2. 궤적 수 제한 (200개로 제한)
+    train_dataset = limit_dataset(train_dataset, 200)
+    val_dataset = limit_dataset(val_dataset, 50)
     
     scaler = Scaler(lowdim_dict=train_dataset.lowdim_shapes)
     scaler.load('/home/jeonchanwook/MTIL/scaler_params.pth')
     train_dataset.scaler = scaler; val_dataset.scaler = scaler
     
-    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=10, pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=10, pin_memory=True)
+    # 3. Trajectory-based Batch Sampling 설정
+    # 1 Iteration = 1 Trajectory가 되도록 설정
+    train_sampler = TrajectoryBatchSampler(train_dataset.cum_lengths, shuffle=True)
+    val_sampler = TrajectoryBatchSampler(val_dataset.cum_lengths, shuffle=False)
+    
+    train_loader = DataLoader(train_dataset, batch_sampler=train_sampler, num_workers=4, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_sampler=val_sampler, num_workers=4, pin_memory=True)
     
     model = LitMambaJEPA(config, scaler)
-    
-    # Configure CSVLogger
     csv_logger = CSVLogger("lightning_logs", name="jepa")
     
-    from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
-    lr_monitor = LearningRateMonitor(logging_interval='epoch')
-    ckpt_cb = ModelCheckpoint(monitor='val_epoch_loss', mode='min', save_last=True, save_top_k=2, filename="jepa-{epoch}")
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    ckpt_cb = ModelCheckpoint(monitor='val_loss', mode='min', save_last=True, save_top_k=2, filename="jepa-{epoch}")
 
     trainer = pl.Trainer(
-        max_epochs=50,
+        max_epochs=5, # 5 Epoch 설정
         accelerator='gpu',
         devices=1,
-        # 빠른 테스트를 위한 설정 (필요에 따라 주석 해제하거나 수치를 조절하세요)
-        limit_train_batches=2000,  # 200 프레임만 학습 (약 1~2개 궤적)
-        limit_val_batches=400,     # 50 프레임만 검증
         logger=csv_logger,
         callbacks=[lr_monitor, ckpt_cb],
-        precision=32
+        precision=32,
+        gradient_clip_val=1.0
     )
+    
+    print(f"Training started: {len(train_sampler)} trajectories per epoch.")
     trainer.fit(model, train_loader, val_loader)
 
 if __name__ == "__main__":
